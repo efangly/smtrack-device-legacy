1f58eacf8d7a8c0c17de2bc0a4ebc70a57606cd7
FangLy <thefangsni@gmail.com>
2025-08-12 22:44:53 +0700

    feat: update image version in CI workflow and enhance GraphService with dailyResult method

--- src/graph/graph.service.ts
+++ src/graph/graph.service.ts
@@ -5,7 +5,8 @@ import e from 'express';
 
 @Injectable()
 export class GraphService {
-  constructor(private readonly influxdb: InfluxdbService, private readonly redis: RedisService) {}
+  constructor(private readonly influxdb: InfluxdbService, private readonly redis: RedisService) { }
+
   async findAll(sn: string, filter: string) {
     if (!sn) throw new BadRequestException('Invalid sn');
     if (!filter) throw new BadRequestException('Invalid filter');
@@ -48,5 +49,36 @@ export class GraphService {
     const result = await this.influxdb.queryData(query);
     if (result.length > 0) await this.redis.set(`device_legacy_graph:${sn}${filter.split(',').join("")}`, JSON.stringify(result), 1800);
     return result;
+  }
+
+  async dailyResult(date: string) {
+    const reqQuery = `from(bucket: "${process.env.INFLUXDB_BUCKET}") 
+      |> range(start: ${date}T00:00:00Z, stop: ${date}T23:59:59Z) 
+      |> filter(fn: (r) => r._measurement == "templog") 
+      |> filter(fn: (r) => r._field == "temp")
+      |> group(columns: ["sn"])
+      |> timeShift(duration: 7h, columns: ["_time"]) 
+      |> aggregateWindow(every: 5m, fn: count, createEmpty: false)
+      |> filter(fn: (r) => r._value > 5)
+      |> count(column: "_value")`;
+    const notiQuery = `from(bucket: "${process.env.INFLUXDB_BUCKET}") 
+      |> range(start: ${date}T00:00:00Z, stop: ${date}T23:59:59Z) 
+      |> filter(fn: (r) => r._measurement == "templog-alert") 
+      |> filter(fn: (r) => r._field == "message")
+      |> group(columns: ["sn"])
+      |> timeShift(duration: 7h, columns: ["_time"]) 
+      |> count(column: "_value")`;
+    const requests = await this.influxdb.queryData(reqQuery);
+    const notifications = await this.influxdb.queryData(notiQuery);
+    const result: Record<string, { sn: string; freq?: number; notification?: number }> = {};
+    requests.filter((r) => !r.sn.startsWith("TMS")).forEach((r) => { result[r.sn] = { sn: r.sn, freq: r._value }; });
+    notifications.forEach((r) => {
+      if (result[r.sn]) {
+        result[r.sn].notification = r._value;
+      } else {
+        result[r.sn] = { sn: r.sn, notification: r._value };
+      }
+    });
+    return Object.values(result);
   }
 }
