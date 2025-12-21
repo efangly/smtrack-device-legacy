0ed3c477a218773a11ac9d27704cb8bae4612f25
FangLy <thefangsni@gmail.com>
2025-12-21 22:17:46 +0700

    feat: update image version to 1.1.21 in CI workflow and trim whitespace in DevicePayloadDto fields

--- src/common/dto/device-payload.dto.ts
+++ src/common/dto/device-payload.dto.ts
@@ -1,15 +1,19 @@
+import { Transform } from 'class-transformer';
 import { IsString, MaxLength } from 'class-validator';
 
 export class DevicePayloadDto {
   @IsString()
   @MaxLength(150)
+  @Transform(({ value }) => value?.trim())
   id: string;
 
   @IsString()
   @MaxLength(150)
+  @Transform(({ value }) => value?.trim())
   hosId: string;
 
   @IsString()
   @MaxLength(150)
+  @Transform(({ value }) => value?.trim())
   wardId: string;
 }
